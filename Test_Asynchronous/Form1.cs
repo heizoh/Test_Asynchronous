5bab8853d9e433b0b908659a2c962884f3e01bcc
将貴 木村 <mstk0531@hotmail.com>
2023-11-02 09:50:47 +0900

    task問題 少しわかった

--- Test_Asynchronous/Form1.cs
+++ Test_Asynchronous/Form1.cs
@@ -20,42 +20,103 @@ namespace Test_Asynchronous
 
         private string[] strMsg = new string[5];
 
-        private void button1_Click(object sender, EventArgs e)
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            List<Task> tasks = new List<Task>();
+
+            //var nums = Enumerable.Range(0, 5);
+            //foreach (var  x in nums)
+            //{
+            //    Task t = Task.Run(() =>
+            //    {
+            //        heavyTask();
+            //        strMsg[x] = $"Task {x + 1:00} finished\r\n";
+            //        Console.WriteLine(strMsg[x]);
+            //        //this.Invoke((MethodInvoker)(() => textBox1.AppendText(strMsg[x])));
+            //    });
+            //}
+
+            for (int i = 0; i < 5; i++)
+            {
+                var x = i;
+                Task t = Task.Run(() =>
+                {
+                    heavyTask();
+                    strMsg[x] = $"Task {x + 1:00} finished\r\n";
+                    this.Invoke((MethodInvoker)(() => textBox1.AppendText(strMsg[x])));
+                });
+                tasks.Add(t);
+            }
+            await Task.WhenAll(tasks);
+            //Task tt = Task.WhenAll(tasks);
+            //tt.Wait();
+            //for (int i = 0; i < 5; i++) 
+            //{
+            //    textBox1.AppendText(strMsg[i]);
+            //}
+
+            //Task t0 = Task.Run(() =>
+            //{
+            //    heavyTask();
+            //    strMsg[0] = $"Task 01 finished\r\n";
+            //    Console.WriteLine(strMsg[0]);
+            //    this.Invoke((MethodInvoker)(() => textBox1.AppendText(strMsg[0])));
+            //});
+
+            //Task t1 = Task.Run(() =>
+            //{
+            //    heavyTask();
+            //    strMsg[1] = $"Task 02 finished\r\n";
+            //    Console.WriteLine(strMsg[1]);
+            //    this.Invoke((MethodInvoker)(() => textBox1.AppendText(strMsg[1])));
+            //});
+            //Task t2 = Task.Run(() =>
+            //{
+            //    heavyTask();
+            //    strMsg[2] = $"Task 03 finished\r\n";
+            //    Console.WriteLine(strMsg[2]);
+            //    this.Invoke((MethodInvoker)(() => textBox1.AppendText(strMsg[2])));
+            //});
+            //Task t3 = Task.Run(() =>
+            //{
+            //    heavyTask();
+            //    strMsg[3] = $"Task 04 finished\r\n";
+            //    Console.WriteLine(strMsg[3]);
+            //    this.Invoke((MethodInvoker)(() => textBox1.AppendText(strMsg[3])));
+            //});
+            //Task t4 = Task.Run(() =>
+            //{
+            //    heavyTask();
+            //    strMsg[4] = $"Task 05 finished\r\n";
+            //    Console.WriteLine(strMsg[4]);
+            //    this.Invoke((MethodInvoker)(() => textBox1.AppendText(strMsg[4])));
+            //});
+
+            ////Task t = Task.Run(() => done_heavyTasks());
+            ////await t;
+            //await Task.WhenAll(t0, t1, t2, t3, t4);
+            Console.WriteLine("All task fin");
+            textBox1.AppendText("All Proccess Finished");
+
+        }
+
+        private void done_heavyTasks()
         {
             List<Task> tasks = new List<Task>();
 
             var nums = Enumerable.Range(0, 5);
-            foreach (var  x in nums)
+            foreach (var x in nums)
             {
                 Task t = Task.Run(() =>
                 {
                     heavyTask();
                     strMsg[x] = $"Task {x + 1:00} finished\r\n";
                     Console.WriteLine(strMsg[x]);
-                    //this.Invoke((MethodInvoker)(() => textBox1.AppendText(strMsg[x])));
+                    this.Invoke((MethodInvoker)(() => textBox1.AppendText(strMsg[x])));
                 });
             }
-
-            //for (int i = 0; i < 5; i++)
-            //{
-            //    var x = i;
-            //    Task t = Task.Run(() =>
-            //    {
-            //        heavyTask();
-            //        strMsg[x] = $"Task {x + 1:00} finished\r\n";
-            //        this.Invoke((MethodInvoker)(() => textBox1.AppendText(strMsg[x])));
-            //    });
-            //}
-
             Task tt = Task.WhenAll(tasks);
             tt.Wait();
-            for (int i = 0; i < 5; i++) 
-            {
-                textBox1.AppendText(strMsg[i]);
-            }
-            Console.WriteLine("All task fin");
-            textBox1.AppendText("All Proccess Finished");
-
         }
 
         private void heavyTask()
